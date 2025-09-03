945d17205c57dbed18f6d704fd01a28eb0760c6a
riffluv <tomatotabetai69@gmail.com>
2025-09-03 16:15:21 +0900

    tomatotabetai

--- components/ui/GameCard.tsx
+++ components/ui/GameCard.tsx
@@ -208,7 +208,7 @@ export function GameCard({
                   letterSpacing: digits >= 3 ? "-1px" : undefined,
                 }}
               >
-                {typeof number === "number" ? number : (clue || "(連想なし)")}
+                {typeof number === "number" ? number : ""}
               </div>
             </div>
             <div
